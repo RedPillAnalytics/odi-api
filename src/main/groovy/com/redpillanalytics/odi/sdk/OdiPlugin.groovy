d80bda76b38b5b1fc94e92a7e74f9b95cf0bf99f
Stewart Bryson <stewart@redpillanalytics.com>
2019-05-21 04:44:42 -0400

    Initial commit.

---
+++ src/main/groovy/com/redpillanalytics/odi/sdk/OdiPlugin.groovy
@@ -0,0 +1,12 @@
+package com.redpillanalytics.odi.sdk
+
+import groovy.util.logging.Slf4j
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+@Slf4j
+class OdiSdkPlugin implements Plugin<Project> {
+
+   void apply(Project project) {
+   }
+}
